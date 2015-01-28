aedfccc5790e4d8f16edc79f3d14ea9aae709ff7
Kana Natsuno <dev@whileimautomaton.net>
2015-01-28 23:08:51 +0900

    Rename - person => user

--- scheduler.js
+++ scheduler.js
@@ -1,23 +1,23 @@
 angular.module('SchedulerApp', [])
   .controller('SchedulerController', ['$scope', function ($scope) {
-    $scope.people = [
+    $scope.users = [
       {name: 'A', plans: [{name: '1/1', answer: 2}, {name: '2/3', answer: 1}]},
       {name: 'B', plans: [{name: '1/1', answer: 1}, {name: '2/3', answer: 0}]},
       {name: 'C', plans: [{name: '1/1', answer: 0}, {name: '2/3', answer: 2}]}
     ];
 
-    $scope.addPerson = function () {
-      $scope.people.push({
-        name: $scope.newPersonName,
-        plans: $scope.people[0].plans
+    $scope.addUser = function () {
+      $scope.users.push({
+        name: $scope.newUserName,
+        plans: $scope.users[0].plans
                .map(function (p) {return {name: p.name, answer: 0}})
       });
-      $scope.newPersonName = '';
+      $scope.newUserName = '';
     };
 
     $scope.addPlan = function () {
-      $scope.people.forEach(function (p) {
-        p.plans.push({name: $scope.newPlanName, answer: 0});
+      $scope.users.forEach(function (u) {
+        u.plans.push({name: $scope.newPlanName, answer: 0});
       });
       $scope.newPlanName = '';
     };
