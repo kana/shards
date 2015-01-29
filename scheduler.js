e390d90528dfaf40d02d01fc73857d268451bfe4
Kana Natsuno <dev@whileimautomaton.net>
2015-01-29 22:38:01 +0900

    Change data structure - users, plans and answers

--- scheduler.js
+++ scheduler.js
@@ -1,23 +1,53 @@
 angular.module('SchedulerApp', [])
   .controller('SchedulerController', ['$scope', function ($scope) {
     $scope.users = [
-      {name: 'A', plans: [{name: '1/1', answer: 2}, {name: '2/3', answer: 1}]},
-      {name: 'B', plans: [{name: '1/1', answer: 1}, {name: '2/3', answer: 0}]},
-      {name: 'C', plans: [{name: '1/1', answer: 0}, {name: '2/3', answer: 2}]}
+      {id: 1, name: 'A'},
+      {id: 2, name: 'B'},
+      {id: 3, name: 'C'}
+    ];
+    $scope.plans = [
+      {id: 1, name: '1/1'},
+      {id: 2, name: '2/3'}
+    ];
+    $scope.answers = [
+      {userId: 1, planId: 1, ok: 2},
+      {userId: 1, planId: 2, ok: 1},
+      {userId: 2, planId: 1, ok: 1},
+      {userId: 2, planId: 2, ok: 0},
+      {userId: 3, planId: 1, ok: 0},
+      {userId: 3, planId: 2, ok: 2}
     ];
 
+    $scope.answersBy = function (user) {
+      return $scope.answers.filter(function (a) {
+        return a.userId === user.id;
+      });
+    };
+
     $scope.addUser = function () {
+      var newUserId =
+        Math.max.apply(null, $scope.users.map(function (u) {return u.id;})) +
+        1;
       $scope.users.push({
-        name: $scope.newUserName,
-        plans: $scope.users[0].plans
-               .map(function (p) {return {name: p.name, answer: 0}})
+        id: newUserId,
+        name: $scope.newUserName
+      });
+      $scope.plans.forEach(function (p) {
+        $scope.answers.push({userId: newUserId, planId: p.id, ok: 0});
       });
       $scope.newUserName = '';
     };
 
     $scope.addPlan = function () {
+      var newPlanId =
+        Math.max.apply(null, $scope.plans.map(function (p) {return p.id;})) +
+        1;
+      $scope.plans.push({
+        id: newPlanId,
+        name: $scope.newPlanName
+      });
       $scope.users.forEach(function (u) {
-        u.plans.push({name: $scope.newPlanName, answer: 0});
+        $scope.answers.push({userId: u.id, planId: newPlanId, ok: 0});
       });
       $scope.newPlanName = '';
     };
