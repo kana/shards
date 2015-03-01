07c41e440d73fe04689e9a94c5c34deecefcc9f1
Kana Natsuno <dev@whileimautomaton.net>
2015-03-01 22:33:11 +0900

    Define routes

--- angularjs-route-test.js
+++ angularjs-route-test.js
@@ -1,6 +1,20 @@
 angular.module('App', ['ngRoute'])
 .config(function ($routeProvider) {
-  // TODO
+  $routeProvider
+    .when('foo', {
+      controller: 'Partial',
+      templateUrl: 'angularjs-route-test-partial.html',
+      resolve: {
+        name: function () {return 'FoO';}
+      }
+    })
+    .when('bar', {
+      controller: 'Partial',
+      templateUrl: 'angularjs-route-test-partial.html',
+      resolve: {
+        name: function () {return 'bAr';}
+      }
+    });
 })
 .controller('App', function ($scope) {
   // TODO
