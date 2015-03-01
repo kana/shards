9a31dba3cee50b43d5a7244d5549efbf2802f3d8
Kana Natsuno <dev@whileimautomaton.net>
2015-03-01 22:27:52 +0900

    Add templatest to test AngularJS $route

---
+++ angularjs-route-test.js
@@ -0,0 +1,8 @@
+angular.module('App', ['ngRoute'])
+.config(function ($routeProvider) {
+  // TODO
+})
+.controller('App', function ($scope) {
+  // TODO
+});
+// vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
