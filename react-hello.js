2850d8fe15b202f0398a3e590198f218056fef58
Kana Natsuno <dev@whileimautomaton.net>
2015-12-12 11:18:25 +0900

    Replace with timer example

--- react-hello.js
+++ react-hello.js
@@ -1,4 +1,21 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('container')
-);
+'use strict';
+
+var ExampleApplication = React.createClass({
+  render: function() {
+    var elapsed = Math.round(this.props.elapsed  / 100);
+    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+    var message =
+      'React has been successfully running for ' + seconds + ' seconds.';
+
+    return <p>{message}</p>;
+  }
+});
+
+var start = new Date().getTime();
+
+setInterval(function() {
+  ReactDOM.render(
+    <ExampleApplication elapsed={new Date().getTime() - start} />,
+    document.getElementById('container')
+  );
+}, 50);
