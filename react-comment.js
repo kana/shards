dca273fb13a1307732e5b6c8073525c2a824f36f
Kana Natsuno <dev@whileimautomaton.net>
2015-12-13 10:59:03 +0900

    Include some Comments into CommentList

--- react-comment.js
+++ react-comment.js
@@ -10,12 +10,12 @@ var CommentBox = React.createClass({
   }
 });
 
-
 var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
