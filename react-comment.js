d2eda2c49dfe3d08c6dc80badc1a9fd6edcea0b8
Kana Natsuno <dev@whileimautomaton.net>
2015-12-16 19:59:13 +0900

    Load data dynamically

--- react-comment.js
+++ react-comment.js
@@ -1,9 +1,27 @@
 var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {data: []};
+  },
+
+  componentDidMount: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -61,6 +79,6 @@ var data = [
 ];
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 );
