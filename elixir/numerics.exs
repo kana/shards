c284721646714a167704696bda7a11f80d809eb8
Kana Natsuno <dev@whileimautomaton.net>
2016-02-03 20:30:36 +0900

    Numerics in Elixir

---
+++ elixir/numerics.exs
@@ -0,0 +1,15 @@
+IO.puts 10
+IO.puts 0b10
+IO.puts 0o10
+IO.puts 0x10
+IO.puts 1.25
+IO.puts 1 + 2
+IO.puts 5 * 5
+IO.puts 1 / 3
+IO.puts 3 / 3  #=> 1.0
+IO.puts div(11, 3)  #=> 3
+IO.puts rem(11, 3)  #=> 2
+IO.puts round(11 / 3) #=> 4
+IO.puts trunc(11 / 3) #=> 3
+IO.puts 1.0
+IO.puts 1.0e-3
