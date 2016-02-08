3ee0f6e35d8555cf50bf056443b84d74ea9452b7
Kana Natsuno <dev@whileimautomaton.net>
2016-02-08 21:25:26 +0900

    Write cat in Rust

--- rust/cat/src/main.rs
+++ rust/cat/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    while io::stdin().read_line(&mut line).unwrap() > 0 {
+        let _ = io::stdout().write(line.as_bytes());
+        line.clear();
+    }
 }
