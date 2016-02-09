af3317cb18af7bd0fa210c0fa5e67cd401905ee6
Kana Natsuno <dev@whileimautomaton.net>
2016-02-09 19:40:21 +0900

    echo(1) in Rust, first version

--- rust/echo/src/main.rs
+++ rust/echo/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first = true;
+    for arg in env::args() {
+        if first {
+            first = false;
+        } else {
+          print!(" ");
+        }
+        print!("{}", arg);
+    }
+    println!("");
 }
