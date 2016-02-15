709cad87ba3fa3c97110ffc22b9030a914d6ef49
Kana Natsuno <dev@whileimautomaton.net>
2016-02-15 20:06:12 +0900

    Fix type mismatch

--- rust/cat/src/main.rs
+++ rust/cat/src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::fs::File;
-use std::iter;
-use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() {
-    for file_path in env::args().skip(1) {
+    let paths = env::args().skip(1).collect::<Vec<_>>();
+    let paths = if paths.is_empty() { vec!["-".to_string()] } else { paths };
+    for file_path in paths {
         let f = BufReader::new(File::open(file_path).unwrap());
         for line in f.lines() {
             println!("{}", line.unwrap());
