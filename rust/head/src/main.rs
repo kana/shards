68ba89d936e8ec2820f90ae854fdf79917afd600
Kana Natsuno <dev@whileimautomaton.net>
2016-02-19 20:51:35 +0900

    Support -n LINES option

--- rust/head/src/main.rs
+++ rust/head/src/main.rs
@@ -1,8 +1,19 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
 use std::io::prelude::*;
 
 fn main() {
-    // TODO: Support -n LINES
-    let n = 10;
+    let mut opts = Options::new();
+    opts.optopt("n", "", "Number of first lines to output", "LINES");
+    let args: Vec<String> = env::args().collect();
+    let matches = opts.parse(&args[1..]).unwrap();
+
+    let n = match matches.opt_str("n") {
+        Some(s) => { s.parse::<usize>().unwrap() }
+        None => { 10 }
+    };
+
     let stdin = std::io::stdin();
     for line in stdin.lock().lines().take(n) {
         println!("{}", line.unwrap());
