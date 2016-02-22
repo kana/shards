046196667e4900a41872e74451fc1a11bc905b33
Kana Natsuno <dev@whileimautomaton.net>
2016-02-22 19:31:16 +0900

    Count bytes and newlines

--- rust/wc/src/main.rs
+++ rust/wc/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut byte_count = 0;
+    let mut newline_count = 0;
+    for byte in std::io::stdin().bytes() {
+        byte_count = byte_count + 1;
+        if byte.unwrap() == b'\n' {
+            newline_count = newline_count + 1;
+        };
+    }
+    println!("{} {} {}", newline_count, "???", byte_count);
 }
