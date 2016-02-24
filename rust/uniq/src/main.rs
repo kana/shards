04a6654f2de36d98122308dd18b5b806d034723e
Kana Natsuno <dev@whileimautomaton.net>
2016-02-24 19:10:05 +0900

    Implement uniq(1) in Rust

--- rust/uniq/src/main.rs
+++ rust/uniq/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut last_line: String = "".to_string();
+    let mut is_first_line = true;
+
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+
+        if is_first_line || line != last_line {
+            println!("{}", line);
+        }
+
+        last_line = line;
+        is_first_line = false;
+    }
 }
