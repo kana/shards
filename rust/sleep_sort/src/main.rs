67ff0dfb34312765eeb33b842bed297911bfb6df
Kana Natsuno <dev@whileimautomaton.net>
2016-02-25 19:59:41 +0900

    Write sleep sort in Rust

--- rust/sleep_sort/src/main.rs
+++ rust/sleep_sort/src/main.rs
@@ -1,3 +1,44 @@
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
+
+// Example:
+//
+// $ awk '/^[^\/][^\/]/ { print length($0); }' src/main.rs | cargo run
+//      Running `target/debug/sleep_sort`
+// 5
+// 5
+// 10
+// 11
+// 14
+// 16
+// 21
+// 22
+// 24
+// 24
+// 26
+// 33
+// 33
+// 34
+// 35
+// 38
+// 54
+// 62
 fn main() {
-    println!("Hello, world!");
+    let mut handles = Vec::new();
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let n = line.unwrap().parse::<u64>().unwrap();
+
+        handles.push(
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(n * 100));
+                println!("{}", n);
+            })
+        );
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
