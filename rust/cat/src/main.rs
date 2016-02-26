37a7e0b1f777a34ba4dd40ea92066bd1c512ccc3
Kana Natsuno <dev@whileimautomaton.net>
2016-02-26 21:01:09 +0900

    Rust seems to be too restrictive

--- rust/cat/src/main.rs
+++ rust/cat/src/main.rs
@@ -3,41 +3,39 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-// TODO:
-// src/main.rs:10:17: 14:18 error: if and else have incompatible types:
-//  expected `std::io::stdio::StdinLock<'_>`,
-//     found `std::io::buffered::BufReader<std::fs::File>`
-// (expected struct `std::io::stdio::StdinLock`,
-//     found struct `std::io::buffered::BufReader`) [E0308]
-// src/main.rs:10         let f = if file_path == "-" {
-// src/main.rs:11                     std::io::stdin().lock()
-// src/main.rs:12                 } else {
-// src/main.rs:13                     BufReader::new(File::open(file_path).unwrap())
-// src/main.rs:14                 };
-// src/main.rs:10:17: 14:18 help: run `rustc --explain E0308` to see a detailed explanation
-// src/main.rs:15:21: 15:30 error: the type of this value must be known in this context
-// src/main.rs:15         for line in f.lines() {
-//                                    ^~~~~~~~~
-// src/main.rs:16:28: 16:41 error: the type of this value must be known in this context
-// src/main.rs:16             println!("{}", line.unwrap());
-//                                           ^~~~~~~~~~~~~
-// <std macros>:2:25: 2:56 note: in this expansion of format_args!
-// <std macros>:3:1: 3:54 note: in this expansion of print! (defined in <std macros>)
-// src/main.rs:16:13: 16:43 note: in this expansion of println! (defined in <std macros>)
-// error: aborting due to 3 previous errors
+// src/main.rs:47:26: 47:72 error: borrowed value does not live long enough
+// src/main.rs:47                     &mut BufReader::new(File::open(file_path).unwrap()) as &mut BufRead
+//                                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// src/main.rs:48:19: 50:6 note: reference must be valid for the block suffix following statement 0 at 48:18...
+// src/main.rs:48                 };
+// src/main.rs:49         cat(f);
+// src/main.rs:50     }
+// src/main.rs:46:24: 48:18 note: ...but borrowed value is only valid for the expression at 46:23
+// src/main.rs:46                 } else {
+// src/main.rs:47                     &mut BufReader::new(File::open(file_path).unwrap()) as &mut BufRead
+// src/main.rs:48                 };
+// error: aborting due to previous error
 // Could not compile `cat`.
+
+fn cat(f: &mut BufRead) {
+    let mut buffer = String::new();
+    while f.read_line(&mut buffer).unwrap() > 0 {
+        print!("{}", buffer);
+        buffer.clear();
+    }
+}
 
 fn main() {
     let paths = env::args().skip(1).collect::<Vec<_>>();
     let paths = if paths.is_empty() { vec!["-".to_string()] } else { paths };
+    let stdin = std::io::stdin();
+    let mut stdin = stdin.lock();
     for file_path in paths {
         let f = if file_path == "-" {
-                    std::io::stdin().lock()
+                    &mut stdin as &mut BufRead
                 } else {
-                    BufReader::new(File::open(file_path).unwrap())
+                    &mut BufReader::new(File::open(file_path).unwrap()) as &mut BufRead
                 };
-        for line in f.lines() {
-            println!("{}", line.unwrap());
-        }
+        cat(f);
     }
 }
