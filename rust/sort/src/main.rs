8e035c983909a957ccef1103dda27dd55c99fcd5
Kana Natsuno <dev@whileimautomaton.net>
2016-02-26 21:08:06 +0900

    Implement sort(1)

--- rust/sort/src/main.rs
+++ rust/sort/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut lines = Vec::new();
+
+    for line in stdin.lock().lines() {
+        lines.push(line.unwrap());
+    }
+
+    lines.sort();
+    for line in lines {
+        println!("{}", line);
+    }
 }
