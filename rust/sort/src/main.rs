23d38c21a50b63057712f996402e416534e67303
Kana Natsuno <dev@whileimautomaton.net>
2016-03-01 19:39:53 +0900

    It's hard to swap values in Rust

--- rust/sort/src/main.rs
+++ rust/sort/src/main.rs
@@ -1,23 +1,13 @@
 use std::io::prelude::*;
 
-// src/main.rs:7:25: 7:33 error: cannot move out of indexed content [E0507]
-// src/main.rs:7                 let t = lines[i];
-//                                       ^~~~~~~~
-// src/main.rs:7:21: 7:22 note: attempting to move value to here
-// src/main.rs:7                 let t = lines[i];
-//                                   ^
-// src/main.rs:7:21: 7:22 help: to prevent the move, use `ref t` or `ref mut t` to capture value by reference
-// src/main.rs:8:28: 8:36 error: cannot move out of indexed content [E0507]
-// src/main.rs:8                 lines[j] = lines[i];
-//                                          ^~~~~~~~
-
 fn sort(lines: &mut Vec<String>) {
     for i in 0..lines.len() {
         for j in i..lines.len() {
             if lines[i] > lines[j] {
-                let t = lines[i];
-                lines[i] = lines[j];
-                lines[j] = t;
+                // !??!?!?!?!?!
+                unsafe {
+                    std::ptr::swap(&mut lines[i], &mut lines[j]);
+                }
             }
         }
     }
