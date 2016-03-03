f249cc76d4ae1249c89389bb4617b78ae749a31e
Kana Natsuno <dev@whileimautomaton.net>
2016-03-03 19:45:28 +0900

    Write a basic loop

--- rust/rpncalc/src/main.rs
+++ rust/rpncalc/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+
+    for line in stdin.lock().lines() {
+        for token in line.unwrap().split(' ') {
+            println!("{}", token);
+        }
+    }
 }
