83ace123efdf30efd40e64a44bae4ed81d4857cd
Kana Natsuno <dev@whileimautomaton.net>
2016-03-07 19:21:55 +0900

    Test closures - return Vec<_>

--- rust/closure.rs
+++ rust/closure.rs
@@ -1,14 +1,14 @@
-fn add(xs: &Vec<i64>) -> i64 {
-    xs[0] + xs[1]
+fn add(xs: &Vec<i64>) -> Vec<i64> {
+    vec![xs[0] + xs[1]]
 }
 
-fn calc(f: &Fn(&Vec<i64>) -> i64, xs: &Vec<i64>) -> i64 {
+fn calc(f: &Fn(&Vec<i64>) -> Vec<i64>, xs: &Vec<i64>) -> Vec<i64> {
     f(xs)
 }
 
 fn main() {
     let xs = vec![11, 22, 33, 44];
 
-    println!("{}", calc(&add, &xs));
-    println!("{}", calc(&|xs| { xs[0] * xs[2] }, &xs));
+    println!("{:?}", calc(&add, &xs));
+    println!("{:?}", calc(&|xs| { vec![xs[0] * xs[2]] }, &xs));
 }
