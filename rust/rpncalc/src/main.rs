cb78527109ae46987f2892babc7c54ff14843e75
Kana Natsuno <dev@whileimautomaton.net>
2016-03-13 04:41:55 +0900

    Try using closures again

--- rust/rpncalc/src/main.rs
+++ rust/rpncalc/src/main.rs
@@ -1,30 +1,17 @@
 use std::io::prelude::*;
 
-// src/main.rs:41:24: 41:27 error: mismatched types:
-//  expected `for<'r> core::ops::Fn(&'r collections::vec::Vec<i64>) -> collections::vec::Vec<i64> + 'static`,
-//     found `fn(&collections::vec::Vec<i64>) -> collections::vec::Vec<i64> {add}`
-// (expected trait core::ops::Fn,
-//     found fn item) [E0308]
-// src/main.rs:41             calculate: add,
-//                                       ^~~
-// src/main.rs:41:24: 41:27 help: run `rustc --explain E0308` to see a detailed explanation
-// src/main.rs:38:9: 42:10 error: the trait `core::marker::Sized` is not implemented for the type `for<'r> core::ops::Fn(&'r collections::vec::Vec<i64>) -> collections::vec::Vec<i64> + 'static` [E0277]
-// src/main.rs:38         Operator {
-// src/main.rs:39             token: "+",
-// src/main.rs:40             arity: 2,
-// src/main.rs:41             calculate: add,
-// src/main.rs:42         }
-// src/main.rs:38:9: 42:10 help: run `rustc --explain E0277` to see a detailed explanation
-// src/main.rs:38:9: 42:10 note: `for<'r> core::ops::Fn(&'r collections::vec::Vec<i64>) -> collections::vec::Vec<i64> + 'static` does not have a constant size known at compile-time
-// src/main.rs:38:9: 42:10 note: required because it appears within the type `Operator`
-// src/main.rs:38:9: 42:10 note: structs must have a statically known size to be initialized
-// error: aborting due to 2 previous errors
+// Hmm:
+//
+// src/main.rs:32:33: 32:56 error: no method named `calculate` found for type `&Operator<'_>` in the current scope
+// src/main.rs:32                     for v in op.calculate(vec![&l, &r]) {
+//                                                ^~~~~~~~~~~~~~~~~~~~~~~
+// error: aborting due to previous error
 // Could not compile `rpncalc`.
 
-struct Operator {
+struct Operator<'a> {
     token: &'static str,
-    arity: u8,
-    calculate: Fn(&Vec<i64>) -> Vec<i64>,
+    arity: usize,
+    calculate: &'a Fn(&Vec<i64>) -> Vec<i64>,
 }
 
 fn add(ns: &Vec<i64>) -> Vec<i64> {
@@ -38,60 +25,26 @@ fn main() {
         Operator {
             token: "+",
             arity: 2,
-            calculate: add,
+            calculate: &add,
         }
     ];
 
     for line in stdin.lock().lines() {
         for token in line.unwrap().split(' ') {
-            match token {
-                "+" => {
-                    if 2 <= stack.len() {
-                        let r = stack.pop().unwrap();
-                        let l = stack.pop().unwrap();
+            if let Ok(n) = token.parse::<i64>() {
+                stack.push(n);
+            } else if let Some(op) = operators.iter().find(|&op| op.token == token) {
+                if op.arity <= stack.len() {
+                    let r = stack.pop().unwrap();
+                    let l = stack.pop().unwrap();
+                    for v in op.calculate(vec![&l, &r]) {
                         stack.push(l + r);
-                    } else {
-                        println!("Stack does not have enough contents!");
                     }
-                },
-                "-" => {
-                    if 2 <= stack.len() {
-                        let r = stack.pop().unwrap();
-                        let l = stack.pop().unwrap();
-                        stack.push(l - r);
-                    } else {
-                        println!("Stack does not have enough contents!");
-                    }
-                },
-                "*" => {
-                    if 2 <= stack.len() {
-                        let r = stack.pop().unwrap();
-                        let l = stack.pop().unwrap();
-                        stack.push(l * r);
-                    } else {
-                        println!("Stack does not have enough contents!");
-                    }
-                },
-                "/" => {
-                    if 2 <= stack.len() {
-                        let r = stack.pop().unwrap();
-                        let l = stack.pop().unwrap();
-                        stack.push(l / r);
-                    } else {
-                        println!("Stack does not have enough contents!");
-                    }
-                },
-                x => {
-                    let n = x.parse::<i64>();
-                    match n {
-                        Ok(n) => {
-                            stack.push(n);
-                        },
-                        Err(_) => {
-                            println!("I don't know what {} means.", token);
-                        },
-                    }
-                },
+                } else {
+                    println!("Stack does not have enough contents!");
+                }
+            } else {
+                println!("I don't know what {} means.", token);
             }
         }
 
