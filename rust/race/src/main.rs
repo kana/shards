0b82ab0482a066693892b72e6b2e1d37c0bfbd4e
Kana Natsuno <dev@whileimautomaton.net>
2016-03-19 22:17:47 +0900

    Test threaded output

--- rust/race/src/main.rs
+++ rust/race/src/main.rs
@@ -1,3 +1,20 @@
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut handles = Vec::new();
+
+    for thread_id in 0..10 {
+        handles.push(
+            thread::spawn(move || {
+                thread::sleep(Duration::from_millis(100));
+                println!("tid={}", thread_id);
+            })
+        );
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
