9804c9aedfe95ca4656abfad3950c1dfc2ce5f62
Kana Natsuno <dev@whileimautomaton.net>
2016-03-22 21:16:35 +0900

    Parse scripts

--- rust/brainfuck/src/main.rs
+++ rust/brainfuck/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut ops = Vec::new();
+    for file_path in env::args().skip(1) {
+        for b in File::open(file_path).unwrap().bytes() {
+            let c = std::char::from_u32(u32::from(b.unwrap())).unwrap();
+            match c {
+                '<' | '>' | '+' | '-' | ',' | '.' | '[' | ']' => ops.push(c),
+                _ => {},
+            }
+        }
+    }
 }
