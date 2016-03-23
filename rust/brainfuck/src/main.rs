92cdaea6b2959394260ceef2903b900edb801108
Kana Natsuno <dev@whileimautomaton.net>
2016-03-23 21:30:15 +0900

    Write interpreter without ","

--- rust/brainfuck/src/main.rs
+++ rust/brainfuck/src/main.rs
@@ -4,6 +4,11 @@ use std::io::prelude::*;
 
 fn main() {
     let mut ops = Vec::new();
+    let mut ops_index : usize = 0;
+    let mut loop_indices = Vec::new();
+    let mut data = vec![0u32];
+    let mut data_index = 0;
+
     for file_path in env::args().skip(1) {
         for b in File::open(file_path).unwrap().bytes() {
             let c = std::char::from_u32(u32::from(b.unwrap())).unwrap();
@@ -12,5 +17,70 @@ fn main() {
                 _ => {},
             }
         }
+    }
+
+    while ops_index < ops.len() {
+        println!("opi = {:?}; op = {:?}; di = {:?}; data = {:?}", ops_index, ops[ops_index], data_index, data);
+        let c = ops[ops_index];
+        match c {
+            '<' => {
+                if data_index == 0 {
+                    panic!("No more data to <");
+                }
+                data_index = data_index - 1;
+            },
+            '>' => {
+                if data_index == data.len() - 1 {
+                    data.push(0);
+                }
+                data_index = data_index + 1;
+            },
+            '+' => {
+                data[data_index] = data[data_index] + 1;
+            },
+            '-' => {
+                data[data_index] = data[data_index] - 1;
+            },
+            ',' => {
+                // TODO
+            },
+            '.' => {
+                print!("{}", std::char::from_u32(data[data_index]).unwrap());
+            },
+            '[' => {
+                if data[data_index] == 0 {
+                    let mut nest = 0;
+                    let i = ops_index;
+                    for i in ops_index..ops.len() {
+                        match ops[i] {
+                            '[' => {
+                                nest = nest + 1;
+                            },
+                            ']' => {
+                                nest = nest - 1;
+                                if nest == 0 {
+                                    break;
+                                }
+                            }
+                            _ => {},
+                        }
+                    }
+                    ops_index = i;
+                } else {
+                    loop_indices.push(ops_index);
+                }
+            },
+            ']' => {
+                if data[data_index] != 0 {
+                    ops_index = loop_indices.pop().unwrap();
+                    continue;
+                }
+            },
+            _ => {
+                panic!("Unknown instruction: {}", c);
+            },
+        }
+
+        ops_index += 1;
     }
 }
