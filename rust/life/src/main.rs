8e81e398865cb9b73e6d800559d87ad1d98f5448
Kana Natsuno <dev@whileimautomaton.net>
2016-03-27 17:25:27 +0900

    Write renderer

--- rust/life/src/main.rs
+++ rust/life/src/main.rs
@@ -1,3 +1,20 @@
+const WIDTH: usize = 72;
+const HEIGHT: usize = 20;
+
+fn pos(x: usize, y: usize) -> usize {
+    x + y * WIDTH
+}
+
+fn show_board(cells: &[u8]) {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            print!("{}", if cells[pos(x, y)] != 0 { 'o' } else { ' ' });
+        }
+        println!("");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cells = [0; WIDTH * HEIGHT];
+    show_board(&cells);
 }
