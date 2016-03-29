a6bbfb8c0ef27b360bb6abce00562a949350b610
Kana Natsuno <dev@whileimautomaton.net>
2016-03-29 20:20:42 +0900

    Fix updater

--- rust/life/src/main.rs
+++ rust/life/src/main.rs
@@ -20,10 +20,13 @@ fn show_board(cells: &[u8]) {
 
 fn update(cells: &mut [u8]) {
     let mut next = [0; (WIDTH + 2) * (HEIGHT + 2)];
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            for dx in 0..3 {
-                for dy in 0..3 {
+    for dx in 0..3 {
+        for dy in 0..3 {
+            if dx == 1 && dy == 1 {
+                continue;
+            }
+            for y in 0..HEIGHT {
+                for x in 0..WIDTH {
                     if cells[pos(x, y)] != 0 {
                         let i = (x + dx) + (y + dy) * (WIDTH + 2);
                         next[i] = next[i] + 1;
@@ -37,7 +40,7 @@ fn update(cells: &mut [u8]) {
         for x in 0..WIDTH {
             let i = (x + 1) + (y + 1) * (WIDTH + 2);
             cells[pos(x, y)] =
-                if next[i] == 3 || next[i] == 4 { 1 }
+                if next[i] == 2 && cells[pos(x, y)] != 0 || next[i] == 3 { 1 }
                 else { 0 };
         }
     }
@@ -50,6 +53,7 @@ fn main() {
     cells[pos(WIDTH / 2 - 1, HEIGHT / 2)] = 1;
     cells[pos(WIDTH / 2, HEIGHT / 2)] = 1;
     cells[pos(WIDTH / 2, HEIGHT / 2 + 1)] = 1;
+
     loop {
         show_board(&cells);
         update(&mut cells);
