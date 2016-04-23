ef11a9bf1ce7d178528b3dae79de14a4a3bf3e5e
Kana Natsuno <dev@whileimautomaton.net>
2016-04-23 15:57:01 +0900

    Write bouncing ball

--- physicsjs-test/index.js
+++ physicsjs-test/index.js
@@ -1,6 +1,54 @@
 var Physics = require('physicsjs');
 var jQuery = require('jquery');
 
-jQuery(function () {
-  console.log([1, 2, 3, 4, 5].map(x => x * x));
+var world = Physics({
+  timestep: 1000.0 / 160,
+  maxIPF: 16,
+  integrator: 'verlet'
 });
+
+var viewWidth = 500;
+var viewHeight = 300;
+
+var renderer = Physics.renderer('canvas', {
+  el: 'viewport',
+  width: viewWidth,
+  height: viewHeight,
+  meta: false,
+  styles: {
+    'circle': {
+      strokeStyle: 'hsla(60, 37%, 17%, 1)',
+      lineWidth: 1,
+      fillStyle: 'hsla(60, 37%, 57%, 0.8)',
+      angleIndicator: 'hsla(60, 37%, 17%, 0.4)'
+    }
+  }
+});
+world.add(renderer);
+world.on('step', function () {
+  world.render();
+});
+
+var vieiwportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+world.add(Physics.behavior('edge-collision-detection', {
+  aabb: vieiwportBounds,
+  restitution: 0.99,
+  cof: 0.99
+}));
+
+var ball = Physics.body('circle', {
+  x: 50,
+  y: 30,
+  vx: 0.2,
+  vy: 0.01,
+  radius: 20
+});
+world.add(ball);
+
+world.add(Physics.behavior('body-impulse-response'));
+world.add(Physics.behavior('constant-acceleration'));
+
+Physics.util.ticker.on(function (time) {
+  world.step(time);
+});
+Physics.util.ticker.start();
