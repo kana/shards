42ef8efca8a29eb1a8afd33e221f2d67ad0a4348
Kana Natsuno <dev@whileimautomaton.net>
2016-05-30 20:12:37 +0900

    Render game board with a plain HTML

--- othello-react/app.js
+++ othello-react/app.js
@@ -12,9 +12,53 @@ var App = React.createClass({
   render: function () {
     return (
       <div className="app">
-        {this.state.gameTree.player}
-        {this.state.gameTree.moves.length}
+        {this.renderBoard(
+            this.state.gameTree.board,
+            this.state.gameTree.player,
+            this.state.gameTree.moves
+        )}
         <button onClick={this.resetGame}>Start a new game</button>
+      </div>
+    );
+  },
+
+  renderBoard: function (board, player, moves) {
+    var O = Othello;
+    var ss = [];
+    var attackable = [];
+    moves.forEach(function (m) {
+      if (!m.isPassingMove)
+        attackable[O.ix(m.x, m.y)] = true;
+    });
+
+    for (var y = -1; y < O.N; y++) {
+      for (var x = -1; x < O.N; x++) {
+        var key = x + 'x' + y;
+        if (0 <= y && 0 <= x) {
+          var classNames = [];
+          classNames.push('cell');
+          classNames.push(attackable[O.ix(x, y)] ? player : board[O.ix(x, y)]);
+          if (attackable[O.ix(x, y)]) {
+            classNames.push('attackable');
+          }
+          // id = ss.push('cell_' + x + '_' + y);
+          ss.push(
+            <span key={key} className={classNames}>
+            </span>
+          );
+        } else if (0 <= x && y === -1) {
+          ss.push(<span key={key}>{String.fromCharCode('a'.charCodeAt(0) + x)}</span>);
+        } else if (x === -1 && 0 <= y) {
+          ss.push(<span key={key}>{y + 1}</span>);
+        } else /* if (x === -1 && y === -1) */ {
+          ss.push(<span key={key}></span>);
+        }
+      }
+    }
+
+    return (
+      <div className="board">
+        {ss}
       </div>
     );
   },
