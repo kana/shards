e09659d6256cab9042a31d92e07a563f1fc264df
Kana Natsuno <dev@whileimautomaton.net>
2018-04-28 22:53:12 +0900

    Use ts-loader

--- redux-todo/webpack.config.js
+++ redux-todo/webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
   }
 }
