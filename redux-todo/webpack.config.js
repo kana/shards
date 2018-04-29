6833722ef56a1951d00feca13039e06fb8114395
Kana Natsuno <dev@whileimautomaton.net>
2018-04-29 12:34:09 +0900

    Configure to use webpack-dev-server

--- redux-todo/webpack.config.js
+++ redux-todo/webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    contentBase: './dist'
+  },
   entry: './src/index.ts',
   module: {
     rules: [
