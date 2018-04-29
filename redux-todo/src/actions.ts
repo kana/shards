eebaff9fd69fa9e1f64d13d878d217979a775292
Kana Natsuno <dev@whileimautomaton.net>
2018-04-29 14:47:57 +0900

    Make returning type a bit terse

--- redux-todo/src/actions.ts
+++ redux-todo/src/actions.ts
@@ -2,23 +2,23 @@ export const ADD_TODO = 'ADD_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 
-export function addTodo (note: string): { type: typeof ADD_TODO, note: string } {
+export function addTodo (note: string) {
   return {
-    type: ADD_TODO,
+    type: ADD_TODO as typeof ADD_TODO,
     note
   }
 }
 
-export function removeTodo (index: number): { type: typeof REMOVE_TODO, index: number } {
+export function removeTodo (index: number) {
   return {
-    type: REMOVE_TODO,
+    type: REMOVE_TODO as typeof REMOVE_TODO,
     index
   }
 }
 
-export function toggleTodo (index: number): { type: typeof TOGGLE_TODO, index: number } {
+export function toggleTodo (index: number) {
   return {
-    type: TOGGLE_TODO,
+    type: TOGGLE_TODO as typeof TOGGLE_TODO,
     index
   }
 }
